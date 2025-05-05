0393cce1fb2e8f30cb03932817986eee3bffd7f8
AIwithDoug <AIwithDoug@gmail.com>
2025-05-05 09:53:28 +0100

    2 - added some logic info in the comments

--- 2 AddTwoNumber.py
+++ 2 AddTwoNumber.py
@@ -21,4 +21,20 @@ class Solution(object):
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return dummy_head.next
+    
+# setup a dummy, a current to track to location as we cant jump to a certain index.
+# and a carry so we know whats leftover.
+
+# while we have digits left in l1, l2 or in the carry
+# get the value from l1 and l2, if empty it has to be 0
+# or it will try and add None which will error
+
+# add them all together, % by 10 to get the left over.
+# insert the new number (leftover) into the ListNode as a new
+# number, set current.next to the new_node we just made to make
+# sure it is the next in the list and not skipped
+
+# repeat until the lists are done and return dummy_head.next
+# we do that instead of just dummy_head, as its 0 by default
+# we do not want that to be includedß
         
